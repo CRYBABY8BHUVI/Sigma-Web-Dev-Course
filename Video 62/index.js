703cbc4a025537b12f88df10c925ce219af77139
CRYBABY8BHUVI <bhuvijindal583@gmail.com>
2026-01-17 19:28:08 +0530

    excercise 10

--- Video 62/index.js
+++ Video 62/index.js
@@ -15,9 +15,17 @@ Another Word:
 Bros
 Limited
 Hub
-
-
-
 */
+let a = Math.random();
+let b = Math.random();
+let c = Math.random();
+let a1 = ["Crazy","Amazing","Fire"];
+let b1 = ["Engine","Foods","Garments"];
+let c1 = ["Bros","Limited","Hub"];
+let d1 = Math.floor(a*3);
+let d2 = Math.floor(b*3);
+let d3 = Math.floor(c*3);
+
+console.log(a1[d1]+b1[d2]+c1[d3]);
 
 
